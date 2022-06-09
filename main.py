a7502e69d5e0ccf0a09132411ebb545b348a982b
Vybhav <vybhavchaturvedi@gmail.com>
2022-06-10 02:14:24 +0530

    Removed traces of Deepgram

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
 import os
 import asyncio
-from aiohttp import web
-from aiohttp_wsgi import WSGIHandler
-from flask import Flask,jsonify,request
-from typing import Dict, Callable
 
+from flask import Flask, jsonify, request, render_template
 
-
-
-
-load_dotenv()
-
-app = Flask('aioflask')
+app = Flask(__name__)
 
 async def process_audio(fast_socket: web.WebSocketResponse):
     async def get_transcript(data: Dict) -> None:
@@ -22,23 +12,9 @@ async def process_audio(fast_socket: web.WebSocketResponse):
         
             if transcript:
                 await fast_socket.send_str(transcript)
+    return 0
 
 
-
-
-    deepgram_socket = await connect_to_deepgram(get_transcript)
-
-    return deepgram_socket
-
-async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]) -> str:
-    try:
-        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
-        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
-        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
-
-        return socket
-    except Exception as e:
-        raise Exception(f'Could not open socket: {e}')
 
 @app.route('/')
 def index():
@@ -54,6 +30,7 @@ def ReturnJSON():
 
         return jsonify(data)
 
+'''
 async def socket(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request) 
@@ -63,15 +40,9 @@ async def socket(request):
     while True:
         data = await ws.receive_bytes()
         deepgram_socket.send(data)
-
-  
+'''
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    aio_app = web.Application()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='localhost', port=5000, debug=True)
-    wsgi = WSGIHandler(app)
-    aio_app.router.add_route('*', '/{path_info: *}', wsgi.handle_request)
-    aio_app.router.add_route('GET', '/listen', socket)
-    web.run_app(aio_app, port=5555)

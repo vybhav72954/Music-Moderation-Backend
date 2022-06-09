d7a0ed99af6e89ce8f280c0eafbb9ac6b0a99a32
Vybhav <vybhavchaturvedi@gmail.com>
2022-06-10 02:17:00 +0530

    Removed traces of Deepgram

--- main.py
+++ main.py
@@ -5,30 +5,22 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-async def process_audio(fast_socket: web.WebSocketResponse):
-    async def get_transcript(data: Dict) -> None:
-        if 'channel' in data:
-            transcript = data['channel']['alternatives'][0]['transcript']
-        
-            if transcript:
-                await fast_socket.send_str(transcript)
-    return 0
-
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/listen', methods = ['GET'])
+
+@app.route('/listen', methods=['GET'])
 def ReturnJSON():
-    if (request.method=='GET'):
+    if request.method == 'GET':
         data = {
-            "Kidnap":'Influencer',
-            "Subject":"Post Toilet Bowl for me",
+            "Kidnap" : 'Influencer',
+            "Subject" : "Post Toilet Bowl for me",
         }
 
         return jsonify(data)
+
 
 '''
 async def socket(request):
@@ -40,9 +32,18 @@ async def socket(request):
     while True:
         data = await ws.receive_bytes()
         deepgram_socket.send(data)
+
+async def process_audio(fast_socket: web.WebSocketResponse):
+    async def get_transcript(data: Dict) -> None:
+        if 'channel' in data:
+            transcript = data['channel']['alternatives'][0]['transcript']
+        
+            if transcript:
+                await fast_socket.send_str(transcript)
+    return 0
 '''
 
-if __name__ == "__main__":
+if __name__=="__main__":
     loop = asyncio.get_event_loop()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='localhost', port=5000, debug=True)
